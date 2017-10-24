4b01f74c4857aef7764731d63526642bf2fc2034
Alicia Wilking <aliciakaywilking@gmail.com>
2017-10-24 12:06:59 -0500

    pomo timer is functioning on a basic level

--- public/scripts/controllers/do.controller.js
+++ public/scripts/controllers/do.controller.js
@@ -1,120 +1,94 @@
-myApp.controller('DoModeController', function (ItemsService, $location) {
+myApp.controller('DoModeController', function (ItemsService, $location, $interval) {
     console.log('in DoModeController');
+
     var vm = this;
 
-    var app = angular.module("pomodoroApp", []);
-    /* Define a module named pomodoroApp*/
-    
-    /* Defind the angular component*/
-    app.component('pomodoroComp', {
-      bindings: {},
-      templateUrl: '/componentGrid.html',
-      controller : ['$interval',function(interval){
-     
-        var self = this;
-        self.started = false;
-      /* 	
-      started variable stores the state of the timer. If it is true, it means that some task (work, short break, long break) is going on with pomodoro timer. If it is false, it means that the pomodoro is not yet started or it is stopped (by clicking stop button)
-     */
-      
-      self.minutes = 25;
-      /*
-        minutes variable stores current minute which is displayed in the pomodoro.	
-    */
-      
-      self.seconds = 0;
-      /*
-        seconds variable stores current second which is displayed in the pomodoro.
-    */
-      
-      self.fillerIncrement = 200/(self.minutes*60);
-      /*
-        fillerIncrement variable stores the value by which fillerHeight should increase.
-    */
-      
-      self.fillerHeight = 0; 
-      /*
-        fillerHeight variable stores the height of the background filler. Initially it is set to 0. As soon as a pomodoro task starts, its height starts increasing (by a value which is stored in fillerIncrement variable) and it keeps increasing till that particular task ends. On click of stop button, it is again set to 0.
-    
-    */
-    
-      
-      /*
-      resetVariables function will be called for each of the actions (work, Short Break, Long Break, Stop) to set the value of variables to values corresponding to that particular action
-      */
-      
-    self.resetVariables = function(mins, secs, started){
-        self.minutes = mins;
-        self.seconds = secs;
-        self.started = started;
-        self.fillerIncrement = 200/(self.minutes*60);
-        self.fillerHeight = 0;
-    }
-    
-    /* handler to be called when user clicks ‘Work’ button */
-    self.startWork = function(){
-        self.resetVariables(25, 0, true);
-    };
-    
-    /* handler to be called when user clicks ‘Short Break’ button */
-    self.startShortBreak = function(){
-        self.resetVariables(5, 0, true);
-    };
-    
-    /* handler to be called when user clicks ‘Long Break’ button */
-    self.startLongBreak = function(){
-        self.resetVariables(15, 0, true);
-    };
-    
-    /* handler to be called when user clicks ‘Stop’ button */
-    self.stopTimer = function(){
-        self.resetVariables(25, 0, false);
-    };
-    
-    self.timerComplete = function(){
-        self.started = false;
-    };
-    
-    /*
-     intervalCallback function is called each second, updates the values of minutes and seconds variables and reflect the updates in timer
-     */
-    self.intervalCallback = function(){ 
-      if(!self.started) return false;
-        if(self.seconds == 0) {
-            if(self.minutes == 0) {
-                self.timerComplete();
-                return;
+    vm.workTime = 25;
+    vm.restTime = 5;
+    vm.timerCountdown = vm.workTime;
+    vm.workRest = 'work';
+
+
+    var seconds = vm.timerCountdown * 60;
+    var timerRun = false;
+
+    vm.timerCountdown = toTimerOutput(seconds);
+
+    // Changes the timer settings
+    vm.changeTime = function (status, change) {
+        if (status == 'work') {
+            vm.workTime += change;
+            if (vm.workTime <= 0) {
+                vm.workTime = 1;
             }
-            self.seconds = 59;
-            self.minutes--;
+            seconds = vm.workTime * 60;
+            vm.timerCountdown = toTimerOutput(seconds)
         } else {
-            self.seconds--;
+            vm.restTime += change;
+            if (vm.restTime < 0) {
+                vm.restTime = 0;
+            }
         }
-      
-        self.fillerHeight += self.fillerIncrement;
     };
-    
-    /* 
-     toDoubleDigit function converts a number to double digit. If num is single digit (0-9), it is prepended with a ‘0’ and resulting string is returned. If num is double digit, it is returned as such. This function is needed as we want to display ’05’ instead of ‘5’ in timer 
-     */
-    self.toDoubleDigit = function(num){
-        return num < 10 ? '0' + parseInt(num,10) : num;
-    };
-    
-    
-    /*
-     Init method initializes the timer using $interval function such that a callback function (intervalCallback) is called after every second
-     */
-    
-    self.$onInit = function(){
-        self.interval = interval( self.intervalCallback, 1000);
-    /*
-     $onInit is automatically called when component is initialized. interval variable stores the reference id of the currently running interval. If we need to clear the interval, we will need this variable. Although in the demo, we are not clearing the interval, it’s always a good idea to clear the interval at some point.
-     */
-    
-    
-      };
-        
-      }]
-    });
+
+    // Write timer in a readable format
+    function toTimerOutput(time) {
+        var minutes = Math.floor(time / 60);
+        var seconds = time % 60;
+
+        return minutes + ":" + (seconds < 10 ? "0" + seconds : seconds);
+    }
+
+    vm.toggleTimer = function () {
+        if (!timerRun) {
+            if (vm.workRest === 'work') {
+                vm.current = vm.workTime;
+            } else {
+                vm.current = vm.breakTime;
+            }
+            reTimer();
+            timerRun = $interval(reTimer, 1000);
+        } else {
+            $interval.cancel(timerRun);
+            timerRun = false;
+        }
+    }
+
+    vm.reset = function () {
+        vm.workRest = 'work';
+        $interval.cancel(timerRun);
+        timerRun = false;
+        seconds = 60 * vm.workTime;
+        vm.timerCountdown = toTimerOutput(seconds);
+        $('.rest-time').removeClass('active-timer');
+        $('.work-time').removeClass('active-timer');
+    }
+
+    function reTimer() {
+        seconds--;
+        if (vm.workRest === 'rest') {
+            $('.work-time').removeClass('active-timer');
+            $('.rest-time').addClass('active-timer');
+        } else {
+            $('.work-time').addClass('active-timer');
+            $('.rest-time').removeClass('active-timer');
+        }
+        if (seconds < 0) {
+            if (vm.workRest === 'work') {
+                alert("Break time!");
+                vm.workRest = 'rest';
+                seconds = 60 * vm.restTime;
+                vm.timerCountdown = toTimerOutput(seconds);
+            } else {
+                alert("Back to work!")
+                vm.workRest = 'work';
+                seconds = 60 * vm.workTime;
+                vm.timerCountdown = toTimerOutput(seconds);
+            }
+        } else {
+            vm.timerCountdown = toTimerOutput(seconds);
+        }
+    }
+
 });
+
